f3e20bb0251978c55171c63ce52150111ef07826
Josmar Junior <josmar.souzajr@gmail.com>
2015-09-16 01:25:11 -0300

    Criação de método construtor passando o nome do Player e métodos para soma de assassinatos e mortes

--- src/br/com/amil/beans/Player.java
+++ src/br/com/amil/beans/Player.java
@@ -3,25 +3,44 @@ package br.com.amil.beans;
 public class Player {
 
 	private String name;
-	private Integer kills;
-	private Integer deaths;
+	private Integer kills = 0;
+	private Integer deaths = 0;
+	
+	public Player (){}
+	
+	public Player (String name) {
+		this.name = name;
+	}
 	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public Integer getKills() {
 		return kills;
 	}
+	
 	public void setKills(Integer kills) {
 		this.kills = kills;
 	}
+	
 	public Integer getDeaths() {
 		return deaths;
 	}
+	
 	public void setDeaths(Integer deaths) {
 		this.deaths = deaths;
+	}
+	
+	public void addKill() {
+		this.kills++;
+	}
+	
+	public void addDeath() {
+		this.deaths++;
 	}
 }
